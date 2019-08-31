5a3d57452ec64c0319946e66ab4aa7efe9874e77
leon <leon@thentcircle.com>
2019-08-31 20:33:30 +0800

    Initial Commit

---
+++ Pacman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pacman
+//
+//  Created by LeonKang on 2019/8/31.
+//  Copyright © 2019 LeonKang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
